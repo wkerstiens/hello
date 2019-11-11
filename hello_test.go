3804e8aa7574e29c47cd48da7e460db322910eeb
Tony Kerstiens <tony.kerstiens@gmail.com>
2019-11-11 10:32:51 -0700

    final changes

--- hello_test.go
+++ hello_test.go
@@ -3,11 +3,37 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("You")
-	want := "Hello, You"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
 
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Tony", "English")
+		want := "Hello, Tony"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("", "English")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Pepe", "French")
+		want := "Bonjour, Pepe"
+		assertCorrectMessage(t, got, want)
+	})
 }
